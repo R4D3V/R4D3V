04fb6f78494cf1569dfa630c77f0369631cb3b7a
R4DEV <raymonjohns@gmail.com>
2024-01-08 03:28:38 -0800

    master

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,27 +2,48 @@ import reactSvg from "../assets/react.svg";
 import { Link } from "react-router-dom";
 function Header() {
   return (
-    <nav className="container ">
-      <Link to="/" className="">
-         <img
-          src={reactSvg}              alt="Contact 1"
-          className="w-500 h-500 object-cover mb-4 rounded-full"
-      />
-      </Link>
-    <div >
-      <Link to="/Updates">
-        UPDATES
-      </Link>
-      <Link to="/services">
-        SERVICES
-      </Link>
-      <Link to="/contact">
-       CONTACT
-      </Link>
-      <Link to="/about">
-        ABOUT
-      </Link>
-    </div>
+    <nav className=" p-4 flex justify-around">
+      <div className="flex items-center">
+        <Link to="/" className="">
+          <img
+            src={reactSvg}
+            alt="Contact 1"
+            className="w-500 h-500 object-cover mb-4 rounded-full"
+          />
+        </Link>
+      </div>
+      <div className="md:flex flex justify-evenly sm:flex sm:justify-evenly">
+        <Link
+          to="/Updates"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          UPDATES
+        </Link>
+        <Link
+          to="/products"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          SHOP
+        </Link>
+        <Link
+          to="/services"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          SERVICES
+        </Link>
+        <Link
+          to="/about"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          ABOUT
+        </Link>
+        <Link
+          to="/contact"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          CONTACT
+        </Link>
+      </div>
     </nav>
   );
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,18 +1,51 @@
+import reactSvg from "../assets/react.svg";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
-      <nav className="bg-gray-800 p-4 flex justify-around">
-       <div className="flex items-center">
-            <span className="text-white font-bold text-lg ">Your Logo</span>
-          </div>
-          <div className="md:flex flex justify-evenly sm:flex sm:justify-evenly">
-            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">UPDATES</a>
-            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">SERVICES</a>
-            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">SHOP</a>
-            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">ABOUT</a>
-            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">CONTACT</a>
-          </div> 
-      </nav>
+    <nav className=" p-4 flex justify-around">
+      <div className="flex items-center">
+        <Link to="/" className="">
+          <img
+            src={reactSvg}
+            alt="Contact 1"
+            className="w-500 h-500 object-cover mb-4 rounded-full"
+          />
+        </Link>
+      </div>
+      <div className="md:flex flex justify-evenly sm:flex sm:justify-evenly">
+        <Link
+          to="/Updates"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          UPDATES
+        </Link>
+        <Link
+          to="/shop"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          SHOP
+        </Link>
+        <Link
+          to="/services"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          SERVICES
+        </Link>
+        <Link
+          to="/about"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          ABOUT
+        </Link>
+        <Link
+          to="/contact"
+          className=" hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 "
+        >
+          CONTACT
+        </Link>
+      </div>
+    </nav>
   );
 }
 export default About;
