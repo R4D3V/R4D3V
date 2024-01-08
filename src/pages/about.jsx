acbd23b9257f51088b713569eba8f66d736ab7ae
R4DEV <68842269+R4D3V@users.noreply.github.com>
2024-01-08 00:10:55 +0000

    master

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,18 @@
 
 function About() {
   return (
-   <div>
- 
-    <h1>this is our lorem ex</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis et totam, dicta hic voluptatum odit eligendi dolore adipisci cupiditate exercitationem eaque delectus cum provident perspiciatis? Vitae maiores quos nulla. Laboriosam consequuntur qui eligendi ad ipsa, earum repudiandae aut quibusdam hic? Dicta molestiae, reprehenderit quo voluptatem vel magnam ut adipisci in.</p>
-   </div>
+      <nav className="bg-gray-800 p-4 flex justify-around">
+       <div className="flex items-center">
+            <span className="text-white font-bold text-lg ">Your Logo</span>
+          </div>
+          <div className="md:flex flex justify-evenly sm:flex sm:justify-evenly">
+            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">UPDATES</a>
+            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">SERVICES</a>
+            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">SHOP</a>
+            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">ABOUT</a>
+            <a href="#" className="text-white hover:text-gray-300 py-2 md:py-0 md:ml-4 sm:font-bold sm:text-1 p-2 ">CONTACT</a>
+          </div> 
+      </nav>
   );
 }
-
 export default About;
