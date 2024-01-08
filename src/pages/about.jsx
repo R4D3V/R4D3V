2d40578e4fb1325fb6e71180750babedf33eb734
R4DEV <raymonjohns@gmail.com>
2024-01-08 09:59:29 -0800

    master

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -59,54 +59,46 @@ function About() {
         </div>
       </div>
 
-      <div className="p-4 py-6 rounded-lg bg-gray-50 dark:bg-gray-800 md:p-8">
+      <div className="p-6 py-6 rounded-lg  md:p-8 shadow-2xl">
         <div className="container p-2 flex text-center">
-          <h1 className="text-white font-extrabold flex text-center">
+          <h1 className=" font-extrabold flex text-center">
             FEEL FREE TO ASK ANYTHING YOU WISH TO!
           </h1>
         </div>
         <form>
           <div className="-mx-2 md:items-center md:flex">
             <div className="flex-1 px-2">
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                First Name
-              </label>
+              <label className="block mb-2 text-sm ">First Name</label>
               <input
                 type="text"
                 placeholder="John "
-                className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full px-5 py-2.5 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-lg  focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
 
             <div className="flex-1 px-2 mt-4 md:mt-0">
-              <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                Last Name
-              </label>
+              <label className="block mb-2 text-sm">Last Name</label>
               <input
                 type="text"
                 placeholder="Doe"
-                className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full px-5 py-2.5 mt-2 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
           </div>
 
           <div className="mt-4">
-            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-              Email address
-            </label>
+            <label className="block mb-2 text-sm ">Email address</label>
             <input
               type="email"
               placeholder="johndoe@example.com"
-              className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg  focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
 
           <div className="w-full mt-4">
-            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-              Message
-            </label>
+            <label className="block mb-2 text-sm ">Message</label>
             <textarea
-              className="block w-full h-32 px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg md:h-56 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+              className="block w-full h-32 px-5 py-2.5 mt-2 bg-white border border-gray-200 rounded-lg md:h-56 dark:placeholder-gray-600dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
               placeholder="Message"
             ></textarea>
           </div>
