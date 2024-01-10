35fd6fec7e9c7663e40ab2b3bf66104b59788cf1
R4DEV <raymonjohns@gmail.com>
2024-01-10 06:04:14 -0800

    master

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,23 +9,6 @@ export default function Footer() {
   return (
     <>
       <nav className="">
-        <div className=" sm:hidden fixed bottom-0 top-0 flex justify-center w-full p-4">
-          <div className="flex justify-center">
-            <Link to="/" className="mx-5 ">
-              <img src={home} alt="RS" className="w-10 h-10" />
-            </Link>
-            <Link to="/about" className="mx-5 ">
-              <img src={about} alt="RS" className="w-10 h-10" />
-            </Link>
-            <Link to="/contact" className="mx-5 ">
-              <img src={contact} alt="RS" className="w-10 h-10" />
-            </Link>
-            <Link to="/products" className="mx-5 ">
-              <img src={portfolio} alt="RS" className="w-10 h-10" />
-            </Link>
-          </div>
-        </div>
-
         <div className="lg:hidden md:hidden fixed bottom-0 left-0 w-full p-4">
           <div className="flex justify-center">
             <Link to="/" className="mx-5 ">
