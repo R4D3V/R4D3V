5db2b9e7a093f15a91e432a165a53ccc5cf09324
R4DEV <raymonjohns@gmail.com>
2024-01-11 06:29:04 -0800

    master

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -2,8 +2,8 @@ function Services() {
   return (
     <section className="container p-5 mx-auto mt-[88px] lg:py-16">
       <div className="grid grid-cols-1 gap-8 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-        <div className="p-8 space-y-3 border-2 border-indigo-400 rounded-xl">
-          <span className="inline-block text-indigo-500">
+        <div className="p-8 bg-[#7c81b2] shadow-2xl space-y-3 border-2 border-indigo-400 rounded-xl">
+          <span className="inline-block ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-8 h-8"
@@ -17,13 +17,13 @@ function Services() {
           Web Development
           </h1>
 
-          <p className="text-gray-500">
+          <p className="">
           By Designing the layout, user Interface, and finally the functionality, you would be impressed.
           </p>
         </div>
 
-        <div className="p-8 space-y-3 border-2 border-indigo-400 rounded-xl">
-          <span className="inline-block text-indigo-500">
+        <div className="p-8 bg-[#7c81b2] shadow-2xl space-y-3 border-2 border-indigo-400 rounded-xl">
+          <span className="inline-block ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-8 h-8"
@@ -37,12 +37,12 @@ function Services() {
           Responsive Design
           </h1>
 
-          <p className="text-gray-500">
+          <p className="">
           Extremely Responsive website designs suitable for mobile, tablet amd desktop screens.
           </p>
         </div>
-        <div className="p-8 space-y-3 border-2 border-indigo-400 rounded-xl">
-          <span className="inline-block text-indigo-500">
+        <div className="p-8 bg-[#7c81b2] shadow-2xl space-y-3 border-2 border-indigo-400 rounded-xl">
+          <span className="inline-block ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-8 h-8"
@@ -56,11 +56,11 @@ function Services() {
           Microsoft Office
           </h1>
 
-          <p className="text-gray-500">
+          <p className="">
           With 5+ years of working with Microsoft office, am pretty confident about it, if i dont know a few tricks, chances are that I soon will.          </p>
         </div>
-        <div className="p-8 space-y-3 border-2 border-indigo-400 rounded-xl">
-          <span className="inline-block text-indigo-500">
+        <div className="p-8 bg-[#7c81b2] shadow-2xl space-y-3 border-2 border-indigo-400 rounded-xl">
+          <span className="inline-block ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-8 h-8"
@@ -74,7 +74,7 @@ function Services() {
           Graphics Design
           </h1>
 
-          <p className="text-gray-500">
+          <p className="">
           I build brands through cultural insights & strategic vision. Custom crafted business solutions.          </p>
         </div>
         </div>
